3ad46fe906e99abae7cceb7987986122234fba4c
Bohdan <www.lbohdan@gmail.com>
2017-08-13 17:31:49 +0300

    m

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker tag $TRAVIS_REPO_SLUG:$TAG $TRAVIS_REPO_SLUG:$TAG
+#docker tag $TRAVIS_REPO_SLUG:$TAG $TRAVIS_REPO_SLUG:travis-$TRAVIS_BUILD_NUMBER
+docker push $TRAVIS_REPO_SLUG
