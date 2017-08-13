97d6060284f213f4fcb9cfe1ba906f4143891b7d
Bohdan <www.lbohdan@gmail.com>
2017-08-13 11:21:18 +0300

    change test ci

--- server/core/core.go
+++ server/core/core.go
@@ -7,7 +7,7 @@ import (
 )
 
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received Request: ", r.URL.Path)
 
+	log.Println("Received Request: ", r.URL.Path)
 	fmt.Fprintf(w, "Hello World 4!")
 }
