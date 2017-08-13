2c560f3e93ef03a167ccfb20fd7f1ca5666daa1f
Bohdan <www.lbohdan@gmail.com>
2017-08-13 16:50:09 +0300

    asq

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login --username $DOCKER_USER --password $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
@@ -7,4 +7,4 @@ else
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 
-docker push lbohdan/citest:tagname
+docker push $TRAVIS_REPO_SLUG
