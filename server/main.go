4da2e74671533ecb5d4475308a1e8fe2f7594bd5
Bohdan <www.lbohdan@gmail.com>
2017-08-14 10:49:47 +0300

    some change port

--- server/main.go
+++ server/main.go
@@ -13,9 +13,14 @@ func main() {
 	fmt.Printf("%+v\n", "run server")
 	r := mux.NewRouter()
 	r.HandleFunc("/message", core.MessageHandler) // listen message
-	err := http.ListenAndServe(":8080", r)
+	r.HandleFunc("/", tes)                        // listen message
+	err := http.ListenAndServe(":5000", r)
 
 	if err != nil {
 		fmt.Printf("%+v\n", "cannot run server")
 	}
+}
+
+func tes(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "tes GET method")
 }
